dce0fb5a9845c1bbdcd2439267e25ba527daf21a
Paul Lebedev <pavell@realconnex.com>
2018-08-24 15:22:26 +0300

    Deleted constants from main class

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -10,15 +10,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HttpResponse extends Response
 {
-    const F_DATA = 'data';
-    const F_LIMIT = 'limit';
-    const F_PAGE = 'page';
-    const F_OFFSET = 'offset';
-    const F_TOTAL_PAGES = 'total_pages';
-    const F_NEXT_PAGE = 'next_page';
-    const F_PREV_PAGE = 'prev_page';
-    const F_RESULT = 'result';
-
     public $headers = ['Content-Type' => 'application/json'];
 
     protected $statusCode = JsonResponse::HTTP_OK;
